8bde1ead35d6f0c754badd1959f13f388869c729
Longyearbyen-R <benkhelifa.mohamed@esprit.tn>
2023-04-08 08:17:26 +0100

    fix

--- FournisseurService/src/main/java/com/esprit/examen/FournisseurApplicationMS.java
+++ FournisseurService/src/main/java/com/esprit/examen/FournisseurApplicationMS.java
@@ -1,15 +1,73 @@
 package com.esprit.examen;
 
 
+import com.esprit.examen.entities.DetailFournisseur;
+import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.entities.SecteurActivite;
+import com.esprit.examen.repositories.DetailFournisseurRepository;
+import com.esprit.examen.repositories.FournisseurRepository;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class FournisseurApplicationMS {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FournisseurApplicationMS.class, args);
-}
+    public static void main(String[] args) {
+        SpringApplication.run(FournisseurApplicationMS.class, args);
+    }
+
+    @Bean
+    ApplicationRunner start(
+            FournisseurRepository fournisseurRepository,
+            DetailFournisseurRepository detailFournisseurRepository,
+            SecteurActiviteRepository secteurActiviteRepository) {
+        return args -> {
+
+            Fournisseur fournisseur1 = Fournisseur.builder()
+                    .code("156")
+                    .build();
+            fournisseurRepository.save(fournisseur1);
+            Fournisseur fournisseur2 = Fournisseur.builder()
+                    .code("157")
+                    .build();
+            fournisseurRepository.save(fournisseur1);
+
+            DetailFournisseur detailFournisseur1 =
+                    DetailFournisseur.builder()
+                            .email("f2@gmail.com")
+                            .adresse("ben arous")
+                            .matricule("matricule2")
+                            .fournisseur(fournisseur1)
+                            .build();
+
+            DetailFournisseur detailFournisseur2 =
+                    DetailFournisseur.builder()
+                            .email("f3@gmail.com")
+                            .adresse("beja")
+                            .matricule("matricule3")
+                            .fournisseur(fournisseur1)
+                            .build();
+            detailFournisseurRepository.save(detailFournisseur1);
+
+            Set<Fournisseur> fournisseurs = new HashSet<>();
+            fournisseurs.add(fournisseur1);
+            fournisseurs.add(fournisseur2);
+
+            SecteurActivite secteurActivite = SecteurActivite.builder()
+                    .codeSecteurActivite("produit alimentaire")
+                    .libelleSecteurActivite("libelle2")
+                    .fournisseurs(fournisseurs)
+                    .build();
+
+            secteurActiviteRepository.save(secteurActivite);
+        };
+    }
 }
